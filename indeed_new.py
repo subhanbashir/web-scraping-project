f82e75606526af84a6738b77410b5253ca7250c8
Subhan Bashir <subhanbashir347@gmail.com>
2022-01-15 17:32:46 +0100

    main file

--- indeed_new.py
+++ indeed_new.py
@@ -3,42 +3,38 @@ from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 
 def extract_data(url):
+    response = requests.get(url)
+    web_data = BeautifulSoup(response.text,'html.parser')
+    return web_data
+
+def counter(extract_data,urls):
     count=0
-    for i in url:
-        response = requests.get(i)
-        soup = BeautifulSoup(response.text,'html.parser')
-        jobtitles=soup.find_all('h2',class_='jobTitle jobTitle-color-purple')
-        companyname=soup.find_all('span',class_='companyName')
+    for i in urls:
+        jobs=extract_data(i)
+        jobtitles=jobs.find_all('h2',class_='jobTitle jobTitle-color-purple')
+        companyname=jobs.find_all('span',class_='companyName')
         for i in range(0,len(jobtitles)):
             print(jobtitles[i].text)
             print(companyname[i].text)
             print('******************')
             count+=1
-    return count 
-def Link(extract_data_MP,i):
-    print('i am going to extract this link')   
-    return extract_data_MP(i)
-        
-def extract_data_MP(i):
+    return count    
+
+def extract_data_MP(scrap_data,i):
     urls=[i]
-    response = requests.get(i)
-    soup = BeautifulSoup(response.text,'html.parser')
-    pages=soup.find('ul', class_='pagination-list')
+    pages=scrap_data.find('ul', class_='pagination-list')
     for a in pages.find_all('a', href=True):
         x=a['href']
         urls.append('https://de.indeed.com'+ x)
-    b=extract_data(urls)   
+    b=counter(extract_data, urls)   
     return b
 
-def plotting(list_of_jobs,number_of_jobs):
-    print('now i am going to create a plot')       
-    def plot(list_of_jobs,number_of_jobs):
+def plot(list_of_jobs,number_of_jobs):
        fig = plt.figure()
        ax = fig.add_axes([0,0,1,1])
        ax.bar(list_of_jobs,number_of_jobs)
        plt.show()
-    return plot(list_of_jobs,number_of_jobs)
-        
+
 if __name__ == "__main__":
     list_of_url=['https://de.indeed.com/jobs?q=IT&l=deutschland&ts',
              'https://de.indeed.com/jobs?q=Business%20Intelligence&l=Deutschland',
@@ -48,6 +44,7 @@ if __name__ == "__main__":
     list_of_jobs=['IT','BI','data science','marketing']
     number_of_jobs=[]
     for i in list_of_url:
-        jobs= Link(extract_data_MP,i)
-        number_of_jobs.append(jobs)
-    plotting(list_of_jobs,number_of_jobs)
+        scrap_data=extract_data(i)
+        Total_jobs=extract_data_MP(scrap_data,i)
+        number_of_jobs.append(Total_jobs)
+    plot(list_of_jobs,number_of_jobs)
